f4b318e58110d4490f1171eb73f0cfb2b7abd203
devekta525 <ektadev531@gmail.com>
2026-03-13 01:27:59 +0530

    path work

--- frontend/src/utils/imageHelper.ts
+++ frontend/src/utils/imageHelper.ts
@@ -5,7 +5,7 @@ export const getImageUrl = (path: string) => {
     // Remove /api if present in VITE_API_URL just in case, though standard in this project seems to be http://localhost:5000 for static files
     // But axios base URL is .../api.
     // Static files are at http://localhost:5000/uploads
-    let cleanBase;
+    let cleanBase = baseUrl;
     if (baseUrl.includes('localhost')) {
         cleanBase = baseUrl.replace(/\/api$/, '');
     }
